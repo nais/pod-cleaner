75c06a51de3412e2ea63f807db28a9db49facbe5
Kyrre Havik Eriksen <kyrremann@gmail.com>
2020-09-02 12:12:16 +0200

    run ones and exit
    Migrating to cronjob instead of deployment

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-image=docker.pkg.github.com/nais/pod-cleaner/pod-cleaner:0.9
+image=docker.pkg.github.com/nais/pod-cleaner/pod-cleaner:1.0
 
 build:
 	docker build -t ${image} .
--- cleanup.py
+++ cleanup.py
@@ -26,33 +26,29 @@ except kubernetes.config.config_exception.ConfigException as e:
         sys.exit(1)
 
 api = client.CoreV1Api()
-while True:
-    namespaces = api.list_namespace()
-    for namespace in namespaces.items:
-        namespace_name = namespace.metadata.name
+namespaces = api.list_namespace()
+for namespace in namespaces.items:
+    namespace_name = namespace.metadata.name
 
-        if namespace_name in ignored_namespaces:
-            continue
+    if namespace_name in ignored_namespaces:
+        continue
 
-        pods = api.list_namespaced_pod(namespace=namespace_name)
-        for pod in pods.items:
-            print(pod.metadata.name)
+    pods = api.list_namespaced_pod(namespace=namespace_name)
+    for pod in pods.items:
 
-            if pod.status and \
-                    pod.status.container_statuses:
-                for container_status in pod.status.container_statuses:
-                    if not container_status.ready \
-                            and container_status.last_state \
-                            and container_status.last_state.terminated \
-                            and container_status.last_state.terminated.reason \
-                            and container_status.last_state.terminated.reason == 'ContainerCannotRun':
-                        if args.dry_run:
-                            print('dry-run: would have deleted', pod.metadata.name, 'in', pod.metadata.namespace)
-                        else:
-                            try:
-                                print('deleting', pod.metadata.name, 'in', pod.metadata.namespace)
-                                api.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
-                            except ApiException as e:
-                                print('exception while deleting: ', e)
-
-    time.sleep(120)
+        if pod.status and \
+                pod.status.container_statuses:
+            for container_status in pod.status.container_statuses:
+                if not container_status.ready \
+                        and container_status.last_state \
+                        and container_status.last_state.terminated \
+                        and container_status.last_state.terminated.reason \
+                        and container_status.last_state.terminated.reason == 'ContainerCannotRun':
+                    if args.dry_run:
+                        print('dry-run: would have deleted', pod.metadata.name, 'in', pod.metadata.namespace)
+                    else:
+                        try:
+                            print('deleting', pod.metadata.name, 'in', pod.metadata.namespace)
+                            api.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
+                        except ApiException as e:
+                            print('exception while deleting: ', e)
